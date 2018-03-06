94ee5bfacbaa79ca1e81f992974d60d8acf92e04
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-06 21:51:59 +0530

    Internal iterator - with Consumer interface with accept method implemented

--- src/main/java/com/examples/functional/SampleFunctionalLoop.java
+++ src/main/java/com/examples/functional/SampleFunctionalLoop.java
@@ -1,18 +1,27 @@
 package com.examples.functional;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class SampleFunctionalLoop {
 
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    //External iterators
 //    
 //    for(int i = 0; i < numbers.size(); i++) {
 //      System.out.println(numbers.get(i));
 //    }
-    for (int e : numbers) {
-      System.out.println(e);
-    }
+//    for (int e : numbers) {
+//      System.out.println(e);
+//    }
+    //Internal iterators
+    numbers.forEach(new Consumer<Integer>() {
+      @Override
+      public void accept(Integer e) {
+        System.out.println(e);
+      }
+    });
   }
 
 }
