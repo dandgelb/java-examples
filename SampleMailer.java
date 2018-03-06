373a802f13febee1e9b0037723a28c5c5e54a869
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-06 16:35:32 +0530

    making send as higher order function -- not syntactically complete

--- SampleMailer.java
+++ SampleMailer.java
@@ -1,18 +1,21 @@
 import java.util.*;
 import java.util.function.*;
-//Creating fluent interfaces using lamdas - cascade method pattern.
+//Creating fluent interfaces using lamdas
+//also called cascade method pattern -- train wreck pattern -- builder pattern
 class Mailer {
   public static void print(String msg) { System.out.println(msg); }
   public Mailer from(String addr) { print("from"); return this;}
   public Mailer to(String addr) { print("to"); return this;}
   public Mailer subject(String line) { print("subject"); return this;}
   public Mailer body(String msg) { print("body"); return this;}
-  public void send() { System.out.println("sending..."); }
+  public static void send() {
+     System.out.println("sending...");
+  }
 }
 
 public class SampleMailer {
   public static void main(String[] args) {
-    new Mailer()
+    Mailer.send(mailer -> ...)
       .from("builder@agiledeveloper.com")
       .to("venkats@agiledeveloper.com")
       .subject("Your code sucks")
@@ -20,3 +23,7 @@ public class SampleMailer {
       .send();
   }
 }
+
+//making send method as higher order function
+//by passing function as argument to a function -- pass a function get the data returned from a function
+//
