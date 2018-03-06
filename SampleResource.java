5ce4eca532d392a42b469120668c6c7821c353a9
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-06 16:58:39 +0530

    Initialize, operate without resource cleanup

--- SampleResource.java
+++ SampleResource.java
@@ -2,11 +2,17 @@ import java.util.*;
 import java.util.function.*;
 
 class Resource {
-  
+  public Resource() { System.out.println("created..."); }
+  public void op1() { System.out.println("op1"); }
+  public void op2() { System.out.println("op2"); }
+  public void finalize() { System.out.println("cleanup..."); }
 }
 
 public class SampleResource {
   public static void main(String [] args) {
+    Resource resource = new Resource();
+    resource.op1();
+    resource.op2();
 
   }
 }
