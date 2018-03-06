4495744e466f7800c4712d83182480b1b787d561
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-06 13:59:11 +0530

    Filters add filters and reduce function

--- SampleDecoratorCamera.java
+++ SampleDecoratorCamera.java
@@ -1,21 +1,30 @@
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.*;
 import java.awt.*;
 
 @SuppressWarnings("unchecked")
 class Camera {
   private Function<Color, Color> filter;
-  public Camera() {
-    setFilters();
+  public Camera(Function<Color, Color>... filters) {
+    setFilters(filters);
   }
   public void setFilters(Function<Color, Color>... filters) {
-    filter = color -> color;
+    //filter = color -> color;
+    // filter = Stream.of(filters)
+    //                .reduce(initial, lamda)
+    filter = Stream.of(filters)
+                   .reduce(
+                      color -> color,
+                      (theFilters, aFilter) -> theFilters.andThen(aFilter)
+                   );
   }
   public Color snap(Color input) {
     return filter.apply(input);
   }
 }
 
+@SuppressWarnings("unchecked")
 public class SampleDecoratorCamera {
   public static void printSnap(Camera camera) {
     System.out.println(camera.snap(new Color(125, 125, 125)));
