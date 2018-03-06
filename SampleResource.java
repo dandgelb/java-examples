2fea372eb8238e6750ebd423087749868bc68ea2
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-06 17:27:59 +0530

    Execute around method call - separating resource management with the API or original author

--- SampleResource.java
+++ SampleResource.java
@@ -1,19 +1,26 @@
 import java.util.*;
 import java.util.function.*;
-
-class Resource implements AutoCloseable {
-  public Resource() { System.out.println("created..."); }
-  public void op1() { System.out.println("op1"); }
-  public void op2() { System.out.println("op2"); }
-  public void close() { System.out.println("cleanup..."); }
+//Execute around method call pattern  - pre op before methods and post op after methods - Line 22 and 23
+class Resource {
+  private Resource() { System.out.println("created..."); }
+  public Resource op1() { System.out.println("op1"); return this; }
+  public Resource op2() { System.out.println("op2"); return this; }
+  private void close() { System.out.println("cleanup..."); }
+  public static void use(Consumer<Resource> block) {
+    Resource resource = new Resource();
+    try{
+      block.accept(resource);
+    } finally {
+      resource.close();
+    }
+  }
 }
 
 public class SampleResource {
   public static void main(String [] args) {
-    Resource resource = new Resource();
-    resource.op1();
-    resource.op2();
-
+    Resource.use(resource ->
+                  resource.op1()
+                          .op2());
     //problems
       //easy to forget ++ close()
       //exception - close() will not be called - solved with try and finally
